f35257960fbaf616f871af47286c5b3b71c6f98f
ddurai <ddurai@mmci.uni-saarland.de>
2020-02-24 19:39:55 +0100

    Delete export.sh

--- scripts/export.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/MMCI/MS/algorithms/work/SOS/SOS/scripts/KREATION/oases/:$PATH
-export PATH=/MMCI/MS/DeNovoAssembly/work/SOS/SOS/installation/oases/:$PATH
-export PATH=/MMCI/MS/DeNovoAssembly/work/SOS/SOS/installation/velvet/:$PATH
-export PATH=/usr/bin:$PATH
-export PATH=/MMCI/MS/DeNovoAssembly/work/SOS/Software/cd-hit-v4.6.4-2015-0603/:$PATH
